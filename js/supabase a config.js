27c007f627be7a3771c6a899fc7217c5af2e7100
Rafael Ferreira <fereira711@gmail.com>
2026-05-03 14:29:16 -0300

    ajustes na origem e melhorias na distribuicao

---
+++ js/supabase a config.js
@@ -0,0 +1,4 @@
+window.SUPABASE_CONFIG = {
+  url: "https://dallfhhzoibxwcpgagsl.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhbGxmaGh6b2lieHdjcGdhZ3NsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3ODI4NjUsImV4cCI6MjA5MjM1ODg2NX0.ol0MxVpWxHyy3m1A6da_XBf5OLw-wSh2XFKzBpHQGHs"
+};
