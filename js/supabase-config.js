3cf8f259f676f01b454ed6c074ea8733d75de5dc
fereira711-lab <fereira711@gmail.com>
2026-05-03 14:36:51 -0300

    Update Supabase configuration with new URL and anonKey

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: "SUA_URL_AQUI",
-  anonKey: "SUA_CHAVE_ANON_PUBLIC"
+  url: "https://dallfhhzoibxwcpgagsl.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRhbGxmaGh6b2lieHdjcGdhZ3NsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY3ODI4NjUsImV4cCI6MjA5MjM1ODg2NX0.ol0MxVpWxHyy3m1A6da_XBf5OLw-wSh2XFKzBpHQGHs"
 };
